9ac4c68f5f7056be2fe2486b81eaba81df341b22
liuchengyiu <liuchengyiu@163.com>
2023-04-04 16:27:02 +0800

    add income part

--- environment/env.py
+++ environment/env.py
@@ -1,17 +1,57 @@
 from sklearn.preprocessing import PolynomialFeatures
 import joblib
+import numpy as np
+from functools import wraps  
 
+class GMM():
+    def __init__(self, params) -> None:
+        self.mu1 = params['gmm_mu_1']
+        self.mu2 = params['gmm_mu_2']
+        self.std1 = params['gmm_std_1']
+        self.std2 = params['gmm_std_2']
+        self.p1 = params['p1']
+        self.p2 = 1 - params['p1'] # unused
+
+
+    def samples(self, num):
+        r = np.random.uniform(0, 1, num)
+        ind1 = r<=self.p1
+        ind2 = r>self.p1
+        len1 = len(r[ind1])
+        r[ind1] = np.random.normal(self.mu1, self.std1, len1)
+        r[ind2] = np.random.normal(self.mu2, self.std2, num - len1)
+        return r
+    
+    def sample(self):
+        return np.random.normal(self.mu1, self.std1) if np.random.uniform(0, 1) <= self.p1 else np.random.normal(self.mu2, self.std2)
 
 class env:
-    def __init__(self, dataset) -> None:
+    def __init__(self, dataset, age, sex, demographic_features) -> None:
         self.ds = dataset
+        self.age = age
+        self.sex = sex
+        self.demographic_features = demographic_features
         self.income_part = {
             'deterministic_model': joblib.load('./datasets/model/income.pkl'),
             'feature_trans': PolynomialFeatures(degree=3),
-            'persistent_influence_parameter': 1.0005,
-            'sigma_transitory': 0.05,
+            'stochastic_param': self.ds.get_income_param(),
+            'gmm': GMM(self.ds.get_income_param()),
+            'last_permanent_shock': 0
         }
     
 
-    def evlove_income(self, ):
-        return
+    def evlove_income(self):
+        def gen_shock():
+            self.income_part['last_permanent_shock'] = \
+             self.income_part['stochastic_param']['persistent_param'] * self.income_part['last_permanent_shock'] + self.income_part['gmm'].sample()
+            return self.income_part['last_permanent_shock'] + \
+                np.random.normal(self.income_part['stochastic_param']['transitory_shock'][0], self.income_part['stochastic_param']['transitory_shock'][1])
+        
+        if np.random.uniform(0, 1) < self.ds.get_unemployment_rate(self.sex, self.age):
+            self.income_part['last_permanent_shock'] = \
+             self.income_part['stochastic_param']['persistent_param'] * self.income_part['last_permanent_shock'] + self.income_part['gmm'].sample()
+            
+            return 0
+        self.demographic_features[0] = self.age
+        main_part = np.abs(self.income_part['deterministic_model'].predict(self.income_part['feature_trans'].fit_transform([self.demographic_features])))
+        return (main_part * np.exp(gen_shock())) [0]
--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from environment import env
 
 
 
-enviorment = env(DataSet())
+enviorment = env(DataSet(), 20, 1, [20.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,])
+print(enviorment.evlove_income())
+enviorment.age += 1
 
-
+print(enviorment.evlove_income())
+enviorment.age += 1
+print(enviorment.evlove_income())
+enviorment.age += 1
+print(enviorment.evlove_income())
+enviorment.age += 1
+print(enviorment.evlove_income())
